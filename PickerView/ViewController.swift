dc3f5fdae2d0b5f546a3438932104046a4832944
SeokJinMoon <damul1992@gmail.com>
2021-03-11 22:32:32 +0900

    KBO 프로야구단 엠블럼을 활용한 PickerView 활용 예시

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    var imageArray = [UIImage?]()
+    var imageFileName = ["bears.png", "dinos.jpg", "eagles.png", "giants.png", "heroes.png",
+                        "lions.jpg", "tigers.png", "twins.jpg", "wyverns.png", "wiz.jpg"]
+    
+    @IBOutlet var pickerImg: UIPickerView!
+    @IBOutlet var selectedItm: UILabel!
+    @IBOutlet var imgView: UIImageView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageFileName[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imgView.image = imageArray[row]
+        selectedItm.text = imageFileName[row]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in 0..<imageFileName.count {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        selectedItm.text = imageFileName[0]
+        imgView.image = imageArray[0]
+        
     }
-
-
 }
 
